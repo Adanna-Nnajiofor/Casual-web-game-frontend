d51b93b3c45325e5fa462c8f616f035e03470ebe
Adanna-Nnajiofor <adanna.nnajiofor@gmail.com>
2025-05-17 00:18:43 +0100

    changes made

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/playNaij-frontend/'
+  base: "/",
+  server: {
+    proxy: {
+      // Proxy all routes except those ending in common static file extensions
+      "^/(?!.*\\.(?:js|css|png|jpg|jpeg|svg|ico|html))": {
+        target: "https://casual-web-game-platform.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
